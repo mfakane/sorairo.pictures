ee503a190b0832fe848eddf1db0ce2f1f3d2e053
mfakane <509998+mfakane@users.noreply.github.com>
2026-05-03 23:14:01 +0000

    Move website contents to dedicated collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,22 @@
+import type { BlogCategory } from "@/models/BlogCategory";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 import { defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      updated: z.date().optional(),
+      tags: z.array(z.string()).optional(),
+      category: z.enum(["diary", "kb", "notice", "review"] as const satisfies BlogCategory[]),
+    }),
+});
+
+const siteCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/site" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
@@ -33,16 +46,17 @@ const headlineCollection = defineCollection({
       title: z.string(),
       date: z.date(),
       href: z.url(),
-      path: reference("blog").optional(),
+      path: reference("site").optional(),
       image: z.union([
-          image(),
-          z.url(),
-        ])
+        image(),
+        z.url(),
+      ])
         .optional(),
     }),
 });
 
 export const collections = {
   blog: blogCollection,
+  site: siteCollection,
   headline: headlineCollection,
 };
